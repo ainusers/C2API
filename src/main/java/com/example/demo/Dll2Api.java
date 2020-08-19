8811c4d863b0d96c974fe34f2fd38d6e1b9b8873
tianyong <979111986@qq.com>
2020-08-19 11:08:38 +0800

    c2api transform update

--- src/main/java/com/example/demo/Dll2Api.java
+++ src/main/java/com/example/demo/Dll2Api.java
@@ -2,16 +2,24 @@ package com.example.demo;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.Platform;
-
-import java.io.File;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * @Author: tianyong
  * @Date: 2020/8/14 10:39
  * @Description: java调用dll文件暴露restful API接口
  */
+@Controller
 public class Dll2Api {
+
+
+    // window加载库的路径
+    static {
+        System.loadLibrary("sum");
+    }
+
 
     /*
      * @Author: tianyong
@@ -22,20 +30,21 @@ public class Dll2Api {
         // 书写格式：Native.loadLibrary(dll_name,interface.class)
         // 创建动态库实例对象
         CustomLibrary INSTANCE = (CustomLibrary) Native.loadLibrary("sum", CustomLibrary.class);
-        // 书写自定义方法
+        // 书写自定义方法 (自定义方法：似如下写法垒加即可)
         int sum(int x, int y);
     }
 
-    // 加载库的绝对路径
-    static {
-        File file = new File("src\\main\\resources\\static\\sum.dll");
-        System.load(file.getAbsolutePath());
+
+    /*
+     * @Author: tianyong
+     * @Date: 2020/8/14 11:05
+     * @Description:暴露求和rest接口
+     */
+    @ResponseBody
+    @RequestMapping("/service/sum")
+    public Integer getSum(Integer var1,Integer var2){
+        return CustomLibrary.INSTANCE.sum(var1,var2);
     }
 
-    // 主程序入口
-    public static void main(String[] args) {
-        // 调用dll中的函数
-        System.out.println(CustomLibrary.INSTANCE.sum(1,2));;
-    }
 
 }
